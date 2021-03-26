6353fe6c29352493c7925ad170707802428aced0
zhuanghanchen <yueliyang@163.com>
2021-03-26 14:29:56 +0800

    326

--- example2.sol
+++ example2.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
+import "./HillStoneFinance.sol";
+import "./safemath.sol";
  
 contract example2{
-    
+    HillStoneFinance public hsfToken;
+    address public maneger;
     struct IndexValue { 
         uint keyIndex; 
         funder value; 
@@ -18,18 +21,24 @@ contract example2{
     }
     mapping(address  => IndexValue) fundermap;
     betPool  _pool ;
-    uint index = 0;
+    uint index ;
+     function contractexample2() public{
+       hsfToken = HillStoneFinance(0xDA0bab807633f07f013f94DD0E6A4F96F8742B53); //实例化一个token
+       index = 0 ;
+       maneger = 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53;
+    }
+    using SafeMath for uint;
     function placeBet(address fromAdress,uint _betClass,uint betAmount) public payable returns(bool success){
         if(this.contains(fromAdress)) {
             if(_betClass == 1){
                fundermap[fromAdress].value.betHighAmount += betAmount;
-               _pool.highPool += betAmount;
+               _pool.highPool = _pool.highPool.add(betAmount);
               }
             if(_betClass ==2){
                fundermap[fromAdress].value.betLowAmount += betAmount;
-               _pool.lowPool +=betAmount;
+               _pool.lowPool = _pool.lowPool.add(betAmount);
               }
-              return true;
+              return hsfToken.transferFrom(fromAdress,maneger,betAmount);
         }
         else{
             index ++;
@@ -40,7 +49,7 @@ contract example2{
                    betHighAmount : betAmount,
                    betLowAmount : 0
                }); 
-               _pool.highPool += betAmount;
+                _pool.highPool = _pool.highPool.add(betAmount);
               }
             if(_betClass ==2){
                 fundermap[fromAdress].value = funder({
@@ -48,9 +57,9 @@ contract example2{
                    betHighAmount : 0,
                    betLowAmount : betAmount
                }); 
-               _pool.lowPool +=betAmount;
+               _pool.lowPool = _pool.lowPool.add(betAmount);
               }
-             return true;
+             return hsfToken.transferFrom(fromAdress,maneger,betAmount);
         }
     }
     function removeBet(address toAddress,uint _betClass,uint removeAmount) public payable returns(bool success){
@@ -62,15 +71,15 @@ contract example2{
                uint last =  this.getValue(toAddress,1);
                require(last >= removeAmount);
                fundermap[toAddress].value.betHighAmount -= removeAmount;
-               _pool.highPool -= removeAmount;
+               _pool.highPool = _pool.highPool.sub(removeAmount);
               }
             if(_betClass ==2){
                uint last =  this.getValue(toAddress,2);
                require(last >= removeAmount);
                fundermap[toAddress].value.betLowAmount -= removeAmount;
-               _pool.lowPool -= removeAmount;
+                _pool.lowPool = _pool.lowPool.sub(removeAmount);
               }
-              return true;
+              return hsfToken.transferFrom(maneger,toAddress,removeAmount);
         }
     }
     function getHighLevelAmount() public  view returns (uint) {
