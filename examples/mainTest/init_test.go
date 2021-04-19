551a1c09884a2eccb1ecee59b7ce218af6689670
zhuanghanchen <yueliyang@163.com>
2021-04-20 01:18:33 +0800

    testshare

--- examples/mainTest/init_test.go
+++ examples/mainTest/init_test.go
@@ -19,18 +19,26 @@ import (
 )
 
 const myAddress = "0x524467bCc0714a80702E1B6C607017d12799AB18"
-const hsfoAddress = "0x2fb27c45Bc655757a3832DdaDD7ca3cbF492330a"
-const usdtoAddress = "0xBdF42596CF3ef5ADadB9F735c1B3e9e9f5a342fF"
-const inAddress = "0xAcF685c2350DB4CB3726E50e38399bD64b516476"
+const hsfoAddress = "0x9260e717e488442a763a8Ac458D21d50CbCA21Cc"
+const usdtoAddress = "0x73eC527e8Af2853aF6CbcCE134B42EF764b85027"
+const inAddress = "0xd06e29C0C668c368E2B1B3EDCA3A5542A3C800DC"
+
+var ma common.Address
+var ha common.Address
+var ua common.Address
+var ia common.Address
 
 func TestInit(t *testing.T) {
 	hsfAddress, _ := deploy(1)
+	ha = hsfAddress
 	time.Sleep(time.Second * 45)
 	fmt.Println(hsfAddress.Hex())
 	usdtAddress, _ := deploy(2)
+	ua = usdtAddress
 	fmt.Println(usdtAddress.Hex())
 	time.Sleep(time.Second * 45)
 	investorsAddress, _ := deploy(3)
+	ia = investorsAddress
 	fmt.Println(investorsAddress.Hex())
 	client := getClient()
 	time.Sleep(time.Second * 45)
@@ -54,18 +62,26 @@ func TestInit(t *testing.T) {
 	}
 	time.Sleep(time.Second * 30)
 	investorsInstance.InitToken(getAuth(5000000, client), hsfAddress, usdtAddress)
+	time.Sleep(time.Second * 30)
 	//placeBet(10,1,getAuth(3000000,client),hsfAddress,hsfInstance)
 	//placeBet(20,2,getAuth(3000000,client),hsfAddress,hsfInstance)
 }
 
 func TestPAR(t *testing.T) {
+	hsfAddress := common.HexToAddress(hsfoAddress)
 	investorsAddress := common.HexToAddress(inAddress)
 	client := getClient()
-	hsfInstance, err := hsf.NewHsf(investorsAddress, client)
+	auth := getAuth(3000000, client)
+	//	auth := getAuthByPrivateKey(3000000,client,"")
+	hsfInstance, err := hsf.NewHsf(hsfAddress, client)
 	if err != nil {
 		println(err)
 	}
-	_, err = hsfInstance.PlaceAndTransfer(getAuth(3000000, client), investorsAddress, big.NewInt(1), big.NewInt(1))
+	//toAddress := common.HexToAddress("0x524467bCc0714a80702E1B6C607017d12799AB18")
+	//i := new(big.Int)
+	//_, err = fmt.Sscan("1000", i)
+	//	_, err =hsfInstance.Transfer(auth,toAddress,i)
+	_, err = hsfInstance.RemoveAndTransfer(auth, investorsAddress, big.NewInt(1), big.NewInt(150))
 	if err != nil {
 		println(err)
 	}
@@ -74,6 +90,63 @@ func TestPAR(t *testing.T) {
 	//	removeBet(10,1,getAuth(3000000,client),hsfAddress,hsfInstance)
 }
 
+func TestTrans(t *testing.T) {
+	client := getClient()
+	auth := getAuth(300000, client)
+	address := common.HexToAddress(usdtoAddress)
+	toaddress := common.HexToAddress(inAddress)
+	instance, err := usdt.NewUsdt(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key := [32]byte{}
+	value := [32]byte{}
+	copy(key[:], []byte("foo"))
+	copy(value[:], []byte("bar"))
+	tx, err := instance.Transfer(auth, toaddress, big.NewInt(200000))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent:
+}
+func TestShareOut(t *testing.T) {
+	investorsAddress := common.HexToAddress(inAddress)
+	client := getClient()
+	auth := getAuth(3000000, client)
+	//	auth := getAuthByPrivateKey(3000000,client,"")
+	investorsInstance, err := investors.NewInvestors(investorsAddress, client)
+	if err != nil {
+		println(err)
+	}
+	//toAddress := common.HexToAddress("0x524467bCc0714a80702E1B6C607017d12799AB18")
+	//i := new(big.Int)
+	//_, err = fmt.Sscan("1000", i)
+	//	_, err =hsfInstance.Transfer(auth,toAddress,i)
+	_, err = investorsInstance.ShareOut(auth, big.NewInt(100))
+	if err != nil {
+		println(err)
+	}
+	//	placeBet(10,1,getAuth(3000000,client),hsfAddress,hsfInstance)
+	//	placeBet(20,2,getAuth(3000000,client),hsfAddress,hsfInstance)
+	//	removeBet(10,1,getAuth(3000000,client),hsfAddress,hsfInstance)
+}
+func TestBal(t *testing.T) {
+	hsfAddress := common.HexToAddress(hsfoAddress)
+	investorsAddress := common.HexToAddress(inAddress)
+	client := getClient()
+	hsfInstance, err := hsf.NewHsf(hsfAddress, client)
+	if err != nil {
+		println(err)
+	}
+	balance, err := hsfInstance.BalanceOf(nil, investorsAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(balance) // "951"
+}
 func placeBet(value int64, _class int64, opts *bind.TransactOpts, manager common.Address, instance *hsf.Hsf) {
 	instance.PlaceAndTransfer(opts, manager, big.NewInt(value), big.NewInt(_class))
 }
@@ -120,6 +193,36 @@ func getClient() *ethclient.Client {
 }
 func getAuth(gaslimit int, client *ethclient.Client) *bind.TransactOpts {
 	privateKey, err := crypto.HexToECDSA("a30eb8e38d72915cd0dfd399f4f110838c4417f0a18b617d946b0d0636a5a5ae") //私钥
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background()) //燃料价格
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)       // in wei
+	auth.GasLimit = uint64(gaslimit) // in units 燃料限制
+	auth.GasPrice = gasPrice
+	return auth
+}
+func getAuthByPrivateKey(gaslimit int, client *ethclient.Client, privateString string) *bind.TransactOpts {
+	privateKey, err := crypto.HexToECDSA(privateString) //私钥
 	if err != nil {
 		log.Fatal(err)
 	}
