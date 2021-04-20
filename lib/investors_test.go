51ae5ed4701f85ce17181ef2f7c67c2efe82cdf8
terrill <terilscaub@gmail.com>
2021-04-20 16:41:56 +0800

    update apis

--- lib/investors_test.go
+++ lib/investors_test.go
@@ -3,11 +3,16 @@ package investors
 import (
 	"context"
 	"crypto/ecdsa"
+	investors_usdt "entysquare/entycats/mockusdt"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
+	"strings"
 	"testing"
 )
 
@@ -55,4 +60,92 @@ func TestRead(t *testing.T) {
 	//fmt.Println(tx.Hash().Hex()) // 0xdae8ba5444eefdc99f4d45cd0c4f24056cba6a02cefbf78066ef9f4188ff7dc0
 
 	//_ = instance
+}
+
+func Test5(t *testing.T) { //总高度
+	url := "https://rinkeby.infura.io/v3/6c81fb1b66804f0698d49f2ec242afc9"
+	//code:="ETH"
+	client, err := ethclient.Dial(url)
+	fmt.Println(err)
+	//sync, err := client.SyncProgress(context.TODO())
+	//fmt.Println(sync.CurrentBlock)
+	tx, _, err := client.TransactionByHash(context.TODO(), common.HexToHash("0x1a944913aab5420504ead95438acfbb1274aa27f933581231816d3ffbc61e980"))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	d := tx.Data()
+
+	method := d[:4]
+	//addressMethod := d[4:]
+	//address := addressMethod[:32]
+	//amount := addressMethod[32:]
+
+	parsed, err := abi.JSON(strings.NewReader(investors_usdt.InvestorsUsdtABI))
+	if err != nil {
+		panic(err)
+	}
+
+	m, err := parsed.MethodById(method)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(m)
+
+	i, err := parsed.Methods["transfer"].Inputs.Unpack(d[4:])
+	addrInterfi := i[0]
+	valueInterfi := i[1]
+	address := (addrInterfi).(common.Address)
+	value := (valueInterfi).(*big.Int)
+
+	fmt.Println(address.Hex())
+	fmt.Println(value.String())
+	//common.HexToAddress(addrInterfi)
+
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//// Value 参数解码
+	//var addrwant common.Address
+	//if err := parsed.Methods["Value"].Inputs.Unpack(&addrwant, valueInput[4:]); err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("should equals", addrwant == address)
+	//
+	//// Value 返回值解码
+	//var balance *big.Int
+	//var returns = common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000005f5e100")
+	//if err := parsed.Unpack(&balance, "Value", returns); err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Value 返回值", balance)
+	//
+	//
+
+	//i,err := parsed.Unpack("transfer",d)
+	fmt.Println(i)
+
+	//var balance *big.Int
+	//if err := parsed.Unpack(&balance, "Value", returns); err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(method)
+	//fmt.Println(addr.String())
+	//fmt.Println(amountStr)
+	//
+	//
+	//
+	//
+	//res := hexutil.Encode(d)
+	//fmt.Println(res)
+
+	//bytes := crypto.Keccak256([]byte("transfer(address,uint256)"))
+	//bytes := crypto.Keccak256([]byte("placeAndTransfer(address,uint256,uint256)"))
+	//bytes2 := bytes[:8]
+
+	//fmt.Println(hexutil.Encode(bytes2))
+
 }
